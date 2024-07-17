cd5e081a1d1bbdcbd2bfe569794497162e6e4ace
PipeItToDevNull <25917750+PipeItToDevNull@users.noreply.github.com>
2024-07-17 17:46:29 +0000

    function that works in apps

--- api/get/index.js
+++ api/get/index.js
@@ -1,9 +1,31 @@
 const { BlobServiceClient } = require('@azure/storage-blob');
 
+async function streamToBuffer(readableStream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on('data', (data) => {
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on('end', () => {
+            resolve(Buffer.concat(chunks));
+        });
+        readableStream.on('error', reject);
+    });
+}
+
 module.exports = async function (context, req) {
     const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AzureBlobConnectionString);
-    const blobUUID = req.params.uuid;
-    const containerClient = blobServiceClient.getContainerClient('linx-container');
+    const blobUUID = req.query.uuid
+
+    if (!blobUUID) {
+        context.res = {
+            status: 400, // Bad Request
+            body: "Missing uuid in query"
+        };
+        return;
+    }
+
+    const containerClient = blobServiceClient.getContainerClient('linx-container'); //define blob container name
     const blockBlobClient = containerClient.getBlockBlobClient(blobUUID);
 
     try {
@@ -18,17 +40,5 @@ module.exports = async function (context, req) {
             body: `An error occurred downloading the blob: ${err.message}`
         };
     }
-}
 
-async function streamToBuffer(readableStream) {
-    return new Promise((resolve, reject) => {
-        const chunks = [];
-        readableStream.on('data', (data) => {
-            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-        });
-        readableStream.on('end', () => {
-            resolve(Buffer.concat(chunks));
-        });
-        readableStream.on('error', reject);
-    });
 }
