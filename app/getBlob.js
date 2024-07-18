8f9757fd45ed7a0f20d92b1f4f0d4185d7e450e3
PipeItToDevNull <piper@dev0.sh>
2024-07-17 21:37:28 -0400

    add blob.js

---
+++ app/getBlob.js
@@ -0,0 +1,14 @@
+window.onload = async function() {
+    //console.log("Pathname: ", window.location.pathname); // log the pathname to the console for debug
+    const uuid = window.location.pathname.split('/')[1];
+    console.log("UUID: ", uuid);
+    const response = await fetch(`/api/get/?uuid=${uuid}`);
+    console.log(response);
+    if (response.ok) {
+        const text = await response.text();
+        document.getElementById('blobContent').textContent = text;
+    } else {
+        const errorMessage = await response.text();
+        document.getElementById('blobContent').textContent = `API Error ${response.status}`;
+    }
+};
