3e0bb7d3176523a7f85b74c0149b37a8fa1edad5
PipeItToDevNull <25917750+PipeItToDevNull@users.noreply.github.com>
2024-07-24 16:14:16 +0000

    add logs

--- app/getBlob.js
+++ app/getBlob.js
@@ -1,17 +1,20 @@
 window.onload = async function() {
     const uuid = window.location.pathname.split('/')[1];
     console.log("UUID: ", uuid);
+
     const response = await fetch(`/api/get/?uuid=${uuid}`);
     console.log(response);
 
     const contentType = response.headers.get("content-type");
-    console.log(contentType);
+    console.log("Content Type: ", contentType);
 
     if (response.ok) {
         if (contentType === 'text/html') {
+            console.log("Rendering: HTML");
             const html = await response.text();
             document.getElementById('blobContent').innerHTML = html;
         } else {
+            console.log("Rendering: Plain text");
             const text = await response.text();
             document.getElementById('blobContent').textContent = text;
         }
