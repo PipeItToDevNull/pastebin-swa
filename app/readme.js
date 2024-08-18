9c54493c9ecb03a99450eb053ebd783714d7c075
PipeItToDevNull <piper@dev0.sh>
2024-08-18 19:04:18 -0400

    improve

--- app/readme.js
+++ app/readme.js
@@ -1,22 +1,21 @@
-async function loadReadme() {
-    // Fetch the URL from data.json
-    const response = await fetch('env.json');
-    const data = await response.json();
-    const readmeUrl = data.readmeUrl;
+fetch('env.json')
+.then(response => response.json())
+.then(data => {
+    const readmeUrl = data.README_URL;
 
     // Fetch the README.md file from the URL
-    const readmeResponse = await fetch(readmeUrl);
-    const text = await readmeResponse.text();
-
+    return fetch(readmeUrl);
+})
+.then(response => response.text())
+.then(text => {
     // Remove the first header
     const lines = text.split('\n');
     if (lines[0].startsWith('#')) {
         lines.shift();
     }
     const modifiedText = lines.join('\n');
-    
+
     // Sanitize and render the markdown content
     document.getElementById('readme').innerHTML = DOMPurify.sanitize(marked.parse(modifiedText));
-}
-
-document.addEventListener('DOMContentLoaded', loadReadme);
+})
+.catch(error => console.error('Error loading README:', error));
