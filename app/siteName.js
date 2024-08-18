4949124d9effffe8392886db4eb2e0592334d83a
PipeItToDevNull <piper@dev0.sh>
2024-08-18 16:42:39 -0400

    add dom loaded event

--- app/siteName.js
+++ app/siteName.js
@@ -1,13 +1,14 @@
 // Set the sitename anywhere required
 // this loads the name from env.json
-async function fetchSiteName() {
-    const response = await fetch('env.json');
-    const data = await response.json();
-    return data.SITE_NAME;
-}
+document.addEventListener('DOMContentLoaded', async () => {
+    async function fetchSiteName() {
+        const response = await fetch('env.json');
+        const data = await response.json();
+        return data.SITE_NAME;
+    }
 
-// Set the site name using the returned string
-fetchSiteName().then(siteName => {
-    document.getElementById('site-name').innerText = siteName;
-    document.title = siteName;
+    fetchSiteName().then(siteName => {
+        document.getElementById('site-name').innerText = siteName;
+        document.title = siteName;
+    });
 });
