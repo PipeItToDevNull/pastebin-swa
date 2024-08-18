1d12092c3141c94967e727fe16190f970fa4843c
PipeItToDevNull <piper@dev0.sh>
2024-08-18 16:50:21 -0400

    try more changes

--- app/siteName.js
+++ app/siteName.js
@@ -1,14 +1,22 @@
 // Set the sitename anywhere required
 // this loads the name from env.json
-document.addEventListener('DOMContentLoaded', async () => {
-    async function fetchSiteName() {
-        const response = await fetch('env.json');
-        const data = await response.json();
-        return data.SITE_NAME;
-    }
+async function fetchSiteName() {
+    const response = await fetch('env.json');
+    const data = await response.json();
+    return data.SITE_NAME;
+}
 
-    fetchSiteName().then(siteName => {
-        document.getElementById('site-name').innerText = siteName;
-        document.title = siteName;
+async function fetchSiteNameAndSet() {
+    await new Promise(resolve => {
+        const checkHeaderLoaded = setInterval(() => {
+            if (document.getElementById('site-name')) {
+                clearInterval(checkHeaderLoaded);
+                resolve();
+            }
+        }, 100);
     });
-});
+
+    const siteName = await fetchSiteName();
+    document.getElementById('site-name').innerText = siteName;
+    document.title = siteName;
+}
