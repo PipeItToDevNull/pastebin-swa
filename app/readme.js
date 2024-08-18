db7ea9b481325324c9ccb9bcaafe4ee19089822d
PipeItToDevNull <25917750+PipeItToDevNull@users.noreply.github.com>
2024-08-18 18:10:19 -0400

    Render repo readme file (#32)
    * add render readme file
    
    * remove header from readme
    
    * add purify script
    
    * mistakes
    
    * change from pre to div

---
+++ app/readme.js
@@ -0,0 +1,14 @@
+// Fetch the README.md file from GitHub
+fetch('https://raw.githubusercontent.com/PipeItToDevNull/pastebin-swa/main/Readme.md')
+.then(response => response.text())
+.then(text => {
+    // Remove the first header
+    const lines = text.split('\n');
+    if (lines[0].startsWith('#')) {
+        lines.shift();
+    }
+    const modifiedText = lines.join('\n');
+    
+    // Sanitize and render the markdown content
+    document.getElementById('readme').innerHTML = DOMPurify.sanitize(marked.parse(modifiedText));
+});
