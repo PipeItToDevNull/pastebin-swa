792a2b4702803ee13bc4bf0684eec0770012e566
PipeItToDevNull <piper@dev0.sh>
2024-12-15 11:57:49 -0500

    make const not function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Upload from './upload';
@@ -6,26 +7,24 @@ import Paste from './paste';
 
 const SITE_NAME = process.env.REACT_APP_SITE_NAME;
 
-function App() {
-    return (
-        <Router>
-            <Helmet>
-                <title>{SITE_NAME}</title>
-            </Helmet>
-            <div id="container">
-                <div id="header">
-                    <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <h1 id="site_name">{SITE_NAME}</h1>
-                    </a>
-                </div>
-                <Routes>
-                    <Route path="/:uuid" element={<Paste />} />
-                    <Route path="/" element={<Upload />} />
-                </Routes>
-                <Footer />
+const App = () => (
+    <Router>
+        <Helmet>
+            <title>{SITE_NAME}</title>
+        </Helmet>
+        <div id="container">
+            <div id="header">
+                <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <h1 id="site_name">{SITE_NAME}</h1>
+                </a>
             </div>
-        </Router>
-    );
-};
+            <Routes>
+                <Route path="/:uuid" element={<Paste />} />
+                <Route path="/" element={<Upload />} />
+            </Routes>
+            <Footer />
+        </div>
+    </Router>
+);
 
 export default App;
